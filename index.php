9f0ca60a32c5f231139983988a627b18981b3c46
Bob Weichler <bobmasaki@hotmail.com>
2016-09-15 09:59:33 +0200

    login / register link bug fix

--- index.php
+++ index.php
@@ -5,7 +5,11 @@ include_once('logic.php');
 if(isset($_SESSION['logged'])){
 	include_once('templates/home.php');
 }else{
-	include_once('templates/login.php');
+	if($_GET['p'] == register){
+		include_once('templates/register.php');
+	}else{
+		include_once('templates/login.php');
+	}
 }
 
 include_once('templates/footer.php');
